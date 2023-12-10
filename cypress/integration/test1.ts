1526726629624f03fb5e312c81ca082c9600f666
naina04 <46651304+naina04@users.noreply.github.com>
2023-12-10 21:03:44 +0100

    Update test1.ts

--- cypress/integration/test1.ts
+++ cypress/integration/test1.ts
@@ -1,1 +1,6 @@
+// your-test-spec.js
+
+cy.login('your_username', 'your_password');
+cy.selectOptionFromDropdown('#myDropdown', 'Option 1');
+cy.visit('/some-page'); // This will now log additional information as well
 
